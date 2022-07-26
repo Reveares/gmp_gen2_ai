330b567f6a9853c44051889c3e845e8f8fa95af4
Reveares <reveares@protonmail.com>
2022-07-27 00:18:13 +0200

    Update AI every tick

--- src/aiScripts/aiInit.ts
+++ src/aiScripts/aiInit.ts
@@ -18,7 +18,7 @@ export function initAiState(waynet?: revmp.Waynet): AiState {
 
 
     aiEventHandler.initEventHandler()
-    setInterval(updateLoop.updateAll.bind(updateLoop), 50);
+    revmp.on("update", () => updateLoop.updateAll());
     /*const testMonster = new OrcElite()
     console.log("monster id: " + testMonster.id)
     aiStateFunctions.spawnNpc(testMonster, "HAFEN", "NEWWORLD\\NEWWORLD.ZEN")
